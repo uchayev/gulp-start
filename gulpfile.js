90bdb5bbad26609b5878f5b0a9e34deada8eacea
Matvey Phoenix <matvey.phoen1x@yandex.ru>
2018-10-02 22:40:52 +0500

    Еще одни промежуточные изменения

--- gulpfile.js
+++ gulpfile.js
@@ -99,11 +99,27 @@ function libs() {
         .pipe(gulp.dest(paths.libs.dest));
 }
 
+// function templates() {
+//     return gulp
+//         .src(paths.templates.pages)
+//         .pipe(pug({ pretty: true }))
+//         .pipe(gulp.dest(paths.root));
+// }
+
 function templates() {
     return gulp
         .src(paths.templates.pages)
         .pipe(pug({ pretty: true }))
-        .pipe(gulp.dest(paths.root));
+        .pipe(gulp.dest(function (file) {
+            let html = file.contents.toString();
+            let selfClosing = ['img', 'input', 'hr', 'br', 'wbr', 'source', 'area', 'col', 'colgroup', 'meta', 'link'];
+            html = html.replace(/(<[A-Z][A-Z0-9]*\b[^>]*>)([^<>]*)(<\/[A-Z][A-Z0-9]*>)/gi, function (str, start, content, end) {
+                let tag = start.replace(/<|>/gi, '').split(' ')[0];
+                return start + ((selfClosing.indexOf(tag) === -1) ? content.trim() : content) + end;
+            });
+            file.contents = new Buffer(html);
+            return paths.root;
+        }));
 }
 
 function styles() {
