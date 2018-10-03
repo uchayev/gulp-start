d793583e6638dfd00f110ca3b19a326edb1b9da3
Matvey Phoenix <matvey.phoen1x@yandex.ru>
2018-10-04 00:50:44 +0500

    #12 Добавил в сборку bemto

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,8 @@ const gulp = require('gulp'),
     svgSprite = require('gulp-svg-sprite'),
     svgmin = require('gulp-svgmin'),
     cheerio = require('gulp-cheerio'),
-    replace = require('gulp-replace');
+    replace = require('gulp-replace'),
+    prettyHtml = require('gulp-pretty-html');
 
 const paths = {
     root: './build',
@@ -99,27 +100,12 @@ function libs() {
         .pipe(gulp.dest(paths.libs.dest));
 }
 
-// function templates() {
-//     return gulp
-//         .src(paths.templates.pages)
-//         .pipe(pug({ pretty: true }))
-//         .pipe(gulp.dest(paths.root));
-// }
-
 function templates() {
     return gulp
         .src(paths.templates.pages)
-        .pipe(pug({ pretty: true }))
-        .pipe(gulp.dest(function (file) {
-            let html = file.contents.toString();
-            let selfClosing = ['img', 'input', 'hr', 'br', 'wbr', 'source', 'area', 'col', 'colgroup', 'meta', 'link'];
-            html = html.replace(/(<[A-Z][A-Z0-9]*\b[^>]*>)([^<>]*)(<\/[A-Z][A-Z0-9]*>)/gi, function (str, start, content, end) {
-                let tag = start.replace(/<|>/gi, '').split(' ')[0];
-                return start + ((selfClosing.indexOf(tag) === -1) ? content.trim() : content) + end;
-            });
-            file.contents = new Buffer(html);
-            return paths.root;
-        }));
+        .pipe(pug({ pretty: false }))
+        .pipe(prettyHtml())
+        .pipe(gulp.dest(paths.root));
 }
 
 function styles() {
